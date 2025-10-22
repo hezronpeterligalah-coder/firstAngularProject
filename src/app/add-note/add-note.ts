0b97e414a86c5d1bf894defe4c7fc5f2590399b2
ligalahpeterhezron <hezronpeterligalah@gmail.com>
2025-10-22 12:39:24 +0300

    add-note-works

---
+++ src/app/add-note/add-note.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormGroup, FormControl, Validators} from "@angular/forms";
+
+
+@Component({
+  selector: 'app-add-note',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-note.html',
+  styleUrl: './add-note.css'
+})
+export class AddNote {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { NotesList } from './notes-list/notes-list';
+import { AddNote } from './add-note/add-note';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Header, NotesList],
+  imports: [Header, NotesList, AddNote],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
